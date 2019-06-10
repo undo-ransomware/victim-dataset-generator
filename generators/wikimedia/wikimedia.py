05e09a947909b26cb6943a31672f1ec6a997f235
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2019-06-10 15:31:01 +0200

    downloading and sampling from search results
    provides a halfway-efficient way of sampling category intersections using
    a query like `deepcat:CC-Zero deepcat:"Ogg files of music by composer"`.

--- generators/wikimedia/wikimedia.py
+++ generators/wikimedia/wikimedia.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 import time
 from mwclient import Site
+from mwclient.listing import List
 from slugify import slugify
 import os
 import random
@@ -35,12 +36,12 @@ def download_random_file():
 	r = requests.get("https://commons.wikimedia.org/wiki/Special:Random/File")
 	d = re.search('(?<=<title>File:).+?(?= - Wikimedia Commons</title>)', r.text, re.DOTALL)
 	download(d.group())
-	
+
 def download_category_files(category_name):
 	category = site.Categories[category_name]
 	for x in category.members(namespace=6):
 		download(x.page_title)
-	
+
 def download_category_random(category_name):
 	# screenscrape the category frontend since the API doesn't offer a way to randomly
 	# sample a category. fully downloading the category using the API and then sampling
@@ -53,7 +54,25 @@ def download_category_random(category_name):
 	d = re.search('(?<=/wiki/File:).+?$', random.choice(href), re.DOTALL)
 	file = urllib.unquote(d.group()).decode("utf-8")
 	download(file)
-	
+
+def download_search_files(query):
+	for res in List(site, 'search', 'sr', srsearch=query, srnamespace=6, srprop='', srsort='none'):
+		download(re.sub('^File:', '', res['title']))
+
+def download_search_random(query, count):
+	q = site.get('query', list='search', srsearch=query, srlimit=1, srprop='', srsort='none', srnamespace=6)
+	total = q['query']['searchinfo']['totalhits']
+	if total > 10000:
+		print "Limiting sampling to first 10k results of " + str(total)
+		total = 10000
+	else:
+		print "Sampling " + str(total) + " results"
+	for i in range(count):
+		index = random.randrange(total)
+		l = List(site, 'search', 'sr', limit=1, sroffset=index, srsearch=query, srnamespace=6, srprop='', srsort='none')
+		title = l.next()['title']
+		download(re.sub('^File:', '', title))
+
 def main(args):
 	if args[0] == 'random':
 		print 'Downloading ' + args[1] + ' random files.'
@@ -66,11 +85,19 @@ def main(args):
 		print 'Downloading ' + args[2] + ' files from the ' + args[1] + ' category.'
 		for i in range(int(args[2])):
 			download_category_random(args[1])
+	elif args[0] == 'search':
+		print 'Downloading all files matching ' + args[1]
+		download_search_files(args[1])
+	elif args[0] == 'randomsearch':
+		print 'Downloading ' + args[2] + ' files matching ' + args[1]
+		download_search_random(args[1], int(args[2]))
 	else:
 		print "random <number of random files>"
 		print "category <wikimedia category>"
 		print "randomcat <number of random files> <wikimedia category>"
- 
+		print "search <search query>"
+		print "randomsearch <number of random files> <search query>"
+
 if __name__ == "__main__":
     status = main(sys.argv[1:])
     sys.exit(status)

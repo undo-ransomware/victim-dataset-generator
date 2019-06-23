5b3a854a003a1b04e5c6284567cb98d74e85e53c
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2019-06-23 14:29:52 +0200

    show filesize by extension, add proper dependencies

--- victim-dataset/stats/filesize.r
+++ victim-dataset/stats/filesize.r
@@ -14,40 +14,43 @@ pdf('temp.pdf', 20, 14)
 
 mkplot = function(levels, title, range=c(20,73)) {
 	maxk = 0
-	count = 0
-	size = 0
 	for (level in levels) {
 		segment = files[files$ext==level,]
 		maxk = max(segment$count, maxk)
-		count = count + sum(segment$count)
-		size = size + sum(10^(segment$dbb / 10) * segment$count)
 	}
 	maxk = maxk / 1000
-	size = round(size / 1000000)
 	#plot(c(0,105), c(0, 1000*maxk), type='n', axes=F, xlab='filesize', ylab='count')
 	# usable range: 100 bytes to ~20MB. there's a long tail with a few huge files
-	plot(range, c(0, 1000*maxk), type='n', axes=F, xlab='filesize', ylab='count',
-		main=paste(title, ' (', count, ' files, ', size, ' MB)', sep=''))
+	plot(range, c(0, 1000*maxk), type='n', axes=F, xlab='filesize', ylab='count')
 
 	axis(1, 0:109, labels=labels, las=3)
 	abline(v=10*log10(10^(0:10)), col='#cccccc')
-	temp=merge(2:9, 10^(0:10), by=NULL)
+	temp = merge(2:9, 10^(0:10), by=NULL)
 	abline(v=10*log10(temp$x*temp$y), lty=2, col='#cccccc')
 
 	axis(2, at=1000*0:maxk, labels=paste(0:maxk, 'k', sep=''), las=2)
 	abline(h=1000*0:maxk, col='#cccccc')
 	abline(h=0:10 * 100, col='#cccccc', lty=2)
 
-	x=0
+	totalCount = 0
+	totalSize = 0
+	x = 0
+	labels = c()
 	for (level in levels) {
-		segment=files[files$ext==level,]
-		col=colors[1 + x %% length(colors)]
+		segment = files[files$ext==level,]
+		col = colors[1 + x %% length(colors)]
 		lines(segment$dbb, segment$count, type='l', col=col)
-		nonzero=segment[segment$count > 0,]
+		nonzero = segment[segment$count > 0,]
 		points(nonzero$dbb, nonzero$count, type='p', col=col, pch=floor(x/length(colors)) + 1)
-		x=x+1
+		x = x + 1
+		count = sum(segment$count)
+		size = round(sum(10^(segment$dbb / 10) * segment$count) / 1000000)
+		labels = c(labels, paste(level, ' (', count, ' files, ', size, ' MB)', sep=''))
+		totalCount = totalCount + count
+		totalSize = totalSize + size
 	}
-	legend(range[2] - 3, 1000 * maxk, levels, col=colors[1 + 0:x %% length(colors)], lty=1, pch=floor(0:x/length(colors)) + 1)
+	title(main=paste(title, ' (', totalCount, ' files, ', totalSize, ' MB)', sep=''))
+	legend(range[2] - 8, 1000 * maxk, labels, col=colors[1 + 0:x %% length(colors)], lty=1, pch=floor(0:x/length(colors)) + 1)
 }
 
 mkplot(levels(files$ext), 'all files (except prefiltered)')
